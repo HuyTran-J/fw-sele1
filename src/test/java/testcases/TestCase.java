7f95982989236914af68fbe5b3e6fcc2a177f0cc
DESKTOP-CA0O33E\Huy <huyy.tran0801@gmail.com>
2026-05-21 19:23:10 +0700

    Wraper Driver

--- src/test/java/testcases/TestCase.java
+++ src/test/java/testcases/TestCase.java
@@ -26,9 +26,8 @@ import pageobjects.thirdPartySite.MailBoxPage;
 public class TestCase extends TestBase{
 
 @Test
-	public void TC01() {
-		//User can log into Railway with valid username and password
-
+	public void TC01() { //User can log into Railway with valid username and password
+	
 		//Data prepare
 		HomePage homePage = new HomePage();
 		LoginPage loginPage = new LoginPage();
